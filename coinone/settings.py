8c35c23876441c795d1f243d4b5f68a9f77f124c
Wave1994-Hoon <kk03187693@gmail.com>
2020-03-12 23:11:10 +0900

    add sign-up(in) - unittest was done - modify def name spelling - modify test.py code, Jsonresponse -> Httpresponse

--- coinone/settings.py
+++ coinone/settings.py
@@ -38,14 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'account',
-    'exchange'
+    'exchange',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
